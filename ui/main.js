991e03d1576d0c4f60eb5f023e14c677640e7217
ankurlogics <30630807+ankurlogics@users.noreply.github.com>
2017-08-06 18:41:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,11 +6,11 @@ var img = document.getElementById('madi');
 var marginLeft=0;
 function moveRight()
 {
-    marginLeft=marginLeft+5;
+    marginLeft=marginLeft+1;
     img.style.marginLeft=marginLeft+'px';
 }
 img.onclick = function()
 {
-    var interval = setInterval(moveRight, 50);
+    var interval = setInterval(moveRight, 1);
   img.style.marginLeft = '100px';  
 };
