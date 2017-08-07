859db3588947c877fcf23ac9ef8b268a2a3eb6b2
ankurlogics <30630807+ankurlogics@users.noreply.github.com>
2017-08-07 20:51:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,23 @@
-console.log('Loaded!');
+//Counter code
+var button = document.getElementById('counter');
+var counter=0;
+button.onclick =function()
+{
+//Make a request to the counter endpoint
+
+//capture the response and stroe it in a variable
+
+//Render the variable in the correct span
+counter= counter+1
+var span = document.getElementById('count');
+span.innerHTML=counter.toString();
+};
+
+
+
+
+
+/*console.log('Loaded!');
 //change the text of the main-text div
 var element = document.getElementById('main-text');
 element.innerHTML='New value';
@@ -13,4 +32,4 @@ img.onclick = function()
 {
     var interval = setInterval(moveRight, 1);
   img.style.marginLeft = '100px';  
-};
+};*/
