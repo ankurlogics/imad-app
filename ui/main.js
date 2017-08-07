549cff21c260a6d4c9cd9b8e5f4aef5315030267
ankurlogics <30630807+ankurlogics@users.noreply.github.com>
2017-08-07 21:19:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,40 @@
-//Counter code
+//counter code
+var button = document.getElementById('counter');
+button.onclick = function()
+	{
+	   //	Make a request to the counter endpoint
+	   var request = new XMLHttpRequest();
+	   //Capture the response and store it in a variable
+	   request.onreadystatechange = function()
+	   	   {
+		      if(request.readyState === XMLHttpRequest.DONE)
+			{
+			   //Take some action
+			   if(request.status===200)
+			      {
+				var counter = request.responseText;
+				var span = document.getElementById('count');
+				span.innerHTML = counter.toString();
+			      }
+			}
+			//Not done yet
+		   };
+		   //make the request
+		   request.open('GET','http://ankurlogics.imad.hasura-app.io/counter',true);
+		   request.send(null);
+	};
+
+
+
+
+
+
+
+
+
+
+
+/*//Counter code
 var button = document.getElementById('counter');
 var counter=0;
 button.onclick =function()
@@ -11,7 +47,7 @@ var request = new XMLHttpRequest();
 counter= counter+1
 var span = document.getElementById('count');
 span.innerHTML=counter.toString();
-};
+};*/
 
 
 
