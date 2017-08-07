29a69cf9dce57e4944aaab69e400a3a1e7b85027
ankurlogics <30630807+ankurlogics@users.noreply.github.com>
2017-08-07 20:38:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,6 +84,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/counter', function (req, res) {
+    counter= counter+1;
+  res.send(counter.toString());
+});
+
 app.get('/:articleNames', function (req, res) {
     // articleName== article-one
     //articles[articleName]=={}content object for article-one
