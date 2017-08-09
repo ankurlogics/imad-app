2312db266d314a1802623960ed0c2515a4c9f323
ankurlogics <30630807+ankurlogics@users.noreply.github.com>
2017-08-09 20:13:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,8 +23,15 @@ button.onclick = function()
 		   request.open('GET','http://ankurlogics.imad.hasura-app.io/counter',true);
 		   request.send(null);
 	};
-
-
+//submit name
+var nameInput = document.getElementById('name') ;
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function()
+{
+  //make a request to the server and send the name
+  //capture a list of name and render it as list
+};
 
 
 
