aafd204ad6924f2c321e0796a080317068e93dcb
ankurlogics <30630807+ankurlogics@users.noreply.github.com>
2017-08-10 10:20:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -113,7 +113,7 @@ app.get('/submit-name/:name', function (req, res) {
   //Get the name from the request
   var name = req.params.name;
   
-  name.push(name);
+  names.push(name);
   res.send(names);//TODO
   
 });
