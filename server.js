5be3265b886a7084d2a038092008adbed03da856
ankurlogics <30630807+ankurlogics@users.noreply.github.com>
2017-08-14 21:34:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,7 +91,23 @@ var htmlTemplate=
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+var pool = new Pool(config);
+app.get('/test-db', function(req,res)
+{
+  //make a select request  
+  //return a response with a result
+  pool.query('SELECT * FROM user', function(err,result)
+{ 
+   if(err)
+   {
+       res.status(500).send(err.toString());
+   }
+   else
+   {
+       res.send(JSON.stringify(result));
+   }
+});
+});
 var counter=0;
 app.get('/counter', function (req, res) {
     counter= counter+1;
